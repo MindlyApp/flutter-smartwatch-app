daff48e2e8457f331f8bb98a82ba968279bb5448
JuKu <kuenzel.justin@t-online.de>
2022-12-17 13:43:07 +0100

    Add MyRouterApp, add support for routing, see AD-24

---
+++ lib/presentation/my_router_app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:wear/wear.dart';
+
+class MyRouterApp extends StatelessWidget {
+  const MyRouterApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
