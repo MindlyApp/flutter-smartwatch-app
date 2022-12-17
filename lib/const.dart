3d044718803678c44dcde2edbc08b323ffb709a0
JuKu <kuenzel.justin@t-online.de>
2022-12-17 15:07:20 +0100

    Change LoginSelectScreen, see AD-12

---
+++ lib/const.dart
@@ -0,0 +1,5 @@
+/// Shared Preferences Keys
+
+class Const {
+  static const String sharedPrefJWTTokenName = "jwt_token";
+}
--- lib/presentation/login_select_screen.dart
+++ lib/presentation/login_select_screen.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:mind_pro/const.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class LoginSelectScreen extends StatelessWidget {
   const LoginSelectScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    // check, if the user is already logged in, see AD-23
+    SharedPreferences.getInstance().then((value) => {
+          if (value.containsKey(Const.sharedPrefJWTTokenName))
+            {
+              // user is already logged in, because he has a JWT token from the backend
+              // TODO: goto other screen
+            }
+        });
+
     return Container();
   }
 }
