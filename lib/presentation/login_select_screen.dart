38c83bf5d96146fddd599dc2ae9b207cadf9e4a7
JuKu <kuenzel.justin@t-online.de>
2022-12-29 17:21:27 +0100

    Add background gradient for login select screen, see AD-12

--- lib/presentation/login_select_screen.dart
+++ lib/presentation/login_select_screen.dart
@@ -16,6 +16,30 @@ class LoginSelectScreen extends StatelessWidget {
           }
         }());
 
-    return Container();
+    return Scaffold(
+      body: Center(
+        child: Container(
+          decoration: const BoxDecoration(
+              gradient: LinearGradient(
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+            colors: [
+              Colors.blue,
+              Colors.red,
+            ],
+          )),
+          child: const Center(
+            child: Text(
+              'Hello Gradient!',
+              style: TextStyle(
+                fontSize: 48.0,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
