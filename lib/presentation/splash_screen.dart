7b7ad9822ed85bf99bbfe56e9a5b409ca9630b7b
JuKu <kuenzel.justin@t-online.de>
2022-12-29 19:25:07 +0100

    Add google cloud messaging, Resolve AD-3

--- lib/presentation/splash_screen.dart
+++ lib/presentation/splash_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:wear/wear.dart';
 import 'dart:async';
+import 'package:firebase_core/firebase_core.dart';
+import '../firebase_options.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key}) : super();
@@ -14,18 +16,37 @@ class _SplashScreenState extends State<SplashScreen> {
 
   @override
   Widget build(BuildContext context) {
+    return FutureBuilder(
+        future: Firebase.initializeApp(
+          options: DefaultFirebaseOptions.currentPlatform,
+        ),
+        builder: (context, snapshot) {
+          // Check for errors
+          if (snapshot.hasError) {
+            return const Center(child: Text('Something went wrong.'));
+          }
+
+          // Once complete, show your application
+          if (snapshot.connectionState == ConnectionState.done) {
+            // after 1,5 seconds goto login screen, see AD-25
+            _timer = Timer(const Duration(milliseconds: 1500), () {
+              Navigator.pushNamedAndRemoveUntil(
+                  context, '/login', ModalRoute.withName('/login'));
+            });
+          }
+
+          // Otherwise, show something whilst waiting for initialization to complete
+          return createScreen();
+        });
+  }
+
+  Scaffold createScreen() {
     return Scaffold(
       body: Center(
         child: WatchShape(
           builder: (BuildContext context, WearShape shape, Widget? child) {
             double screenWidth = MediaQuery.of(context).size.width;
             double screenHeight = MediaQuery.of(context).size.height;
-
-            // after 1,5 seconds goto login screen, see AD-25
-            _timer = Timer(const Duration(milliseconds: 1500), () {
-              Navigator.pushNamedAndRemoveUntil(
-                  context, '/login', ModalRoute.withName('/login'));
-            });
 
             return Stack(
                 /*decoration: const BoxDecoration(
