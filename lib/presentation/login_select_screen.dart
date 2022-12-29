b0ff77e9f3d4ddf2ebc93082dedc02dac848678a
JuKu <kuenzel.justin@t-online.de>
2022-12-29 17:28:49 +0100

    Add background gradient for login select screen, see AD-12

--- lib/presentation/login_select_screen.dart
+++ lib/presentation/login_select_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:mind_pro/const.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:wear/wear.dart';
 
 class LoginSelectScreen extends StatelessWidget {
   const LoginSelectScreen({Key? key}) : super(key: key);
@@ -18,28 +19,40 @@ class LoginSelectScreen extends StatelessWidget {
 
     return Scaffold(
       body: Center(
-        child: Container(
-          decoration: const BoxDecoration(
-              gradient: LinearGradient(
-            begin: Alignment.topRight,
-            end: Alignment.bottomLeft,
-            colors: [
-              Colors.blue,
-              Colors.red,
-            ],
-          )),
-          child: const Center(
-            child: Text(
-              'Hello Gradient!',
-              style: TextStyle(
-                fontSize: 48.0,
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
+          child: WatchShape(
+        builder: (BuildContext context, WearShape shape, Widget? child) {
+          double screenWidth = MediaQuery.of(context).size.width;
+          double screenHeight = MediaQuery.of(context).size.height;
+
+          return Container(
+            /// see also: https://www.digitalocean.com/community/tutorials/flutter-flutter-gradient
+            decoration: const BoxDecoration(
+                gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [
+                Color(0xFF160068),
+                Color(0xFFFB04FC),
+              ],
+            )),
+            child: const Center(
+              child: Text(
+                'Hello Gradient!',
+                style: TextStyle(
+                  fontSize: 48.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
               ),
             ),
-          ),
+          );
+        },
+        child: AmbientMode(
+          builder: (BuildContext context, WearMode mode, Widget? child) {
+            return const Placeholder();
+          },
         ),
-      ),
+      )),
     );
   }
 }
