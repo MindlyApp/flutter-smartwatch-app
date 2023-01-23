101111bc0c38a7f9dfefea5abbfcffef80168cf5
JuKu <kuenzel.justin@t-online.de>
2023-01-23 03:23:38 +0100

    Add imprint page, see AD-28

--- lib/presentation/imprint_screen.dart
+++ lib/presentation/imprint_screen.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 
+// Import for Android features.
+/// import 'package:webview_flutter_android/webview_flutter_android.dart';
+// Import for iOS features.
+/// import 'package:webview_flutter_wkwebview/webview_flutter_wkwebview.dart';
+
 class ImprintScreen extends StatefulWidget {
   const ImprintScreen({Key? key}) : super(key: key);
 
