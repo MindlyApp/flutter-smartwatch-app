6ab095c3985fe777cb0e72d514b148287062df9c
JuKu <kuenzel.justin@t-online.de>
2023-01-23 02:59:41 +0100

    Add logout, see AD-49

--- lib/presentation/mainmenu/mainmenu.dart
+++ lib/presentation/mainmenu/mainmenu.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+import 'package:mind_pro/domain/usecases/login_usecases.dart';
 import 'package:mind_pro/presentation/background/background_builder.dart';
 import 'package:wear/wear.dart';
 
@@ -61,18 +63,25 @@ class _MainMenuScreenState extends State<MainMenuScreen> {
       children: [
         buildMenuEntry("Training 1", Icons.people, "/product/relaxation"),
         /// buildMenuEntry("Relaxation", Icons.people, "/product/relaxation"),
-        buildMenuEntry("Privacy", Icons.people, "/product/relaxation"),
-        buildMenuEntry("Imprint", Icons.people, "/product/relaxation"),
-        buildMenuEntry("Logout", Icons.people, "/login/logout"),
+        buildMenuEntry("Privacy", Icons.privacy_tip, "/info/data-privacy"),
+        buildMenuEntry("Imprint", Icons.info, "/info/imprint"),
+        buildMenuEntry("Logout", Icons.logout, "/login", () async {
+          print('Logout');
+          await GetIt.I.get<LoginUseCases>().logout();
+        }),
       ],
     );
   }
 
   /// Builds a single menu entry button.
-  Widget buildMenuEntry(String title, IconData iconData, String targetScreenPath) {
+  Widget buildMenuEntry(String title, IconData iconData, String targetScreenPath, [VoidCallback? callback]) {
     return ElevatedButton.icon(
       // <-- ElevatedButton
       onPressed: () {
+        if (callback != null) {
+          callback!.call();
+        }
+
         Navigator.pushNamed(context, targetScreenPath);
       },
       style: MSBackgrounds.createMainMenuButtonStyle(context),
